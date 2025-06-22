66233e2c2e09dd3b7c5fdf6be31a1694581bd374
geetasaini2042 <Geetasaini2042@gmail.com>
2025-06-22 23:23:59 +0530

    Create status_filters.py

---
+++ filters/status_filters.py
@@ -0,0 +1,18 @@
+from pyrogram.filters import Filter
+import json
+
+class StatusFilter(Filter):
+    def __init__(self, status_prefix):
+        self.status_prefix = status_prefix
+
+    async def __call__(self, client, message):
+        user_id = str(message.from_user.id)
+
+        try:
+            with open("/storage/emulated/0/BotBuilder/PYTHON/status_user.json", "r") as f:
+                status_data = json.load(f)
+        except:
+            return False
+
+        current_status = status_data.get(user_id, "")
+        return current_status.startswith(self.status_prefix)
